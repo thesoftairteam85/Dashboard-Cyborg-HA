b045d6798ed34d5325ca848d4c1d5c923c21f474
thesoftairteam85 <thesoftairteam85@gmail.com>
2026-08-22 00:21:24 +0200

    chore: add frontend metadata

---
+++ frontend/manifest.js
@@ -0,0 +1,6 @@
+export const CYBORG_FRONTEND = {
+  version: "0.1.1",
+  tag: "cyborg-dashboard",
+  module: "/cyborg_dashboard/frontend/cyborg-dashboard.js",
+  stylesheet: "/cyborg_dashboard/frontend/cyborg-dashboard.css",
+};
