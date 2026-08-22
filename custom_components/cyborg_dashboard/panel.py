aa9bfd805793f0daa508d67f5ca277c0edce697f
thesoftairteam85 <thesoftairteam85@gmail.com>
2026-08-22 08:37:21 +0200

    chore: bump Cyborg panel version

--- custom_components/cyborg_dashboard/panel.py
+++ custom_components/cyborg_dashboard/panel.py
@@ -25,6 +25,6 @@ async def async_register_panel(hass: HomeAssistant) -> None:
         sidebar_title="Cyborg Dashboard",
         sidebar_icon="mdi:view-dashboard-edit",
         module_url=f"{STATIC_PATH}/cyborg-dashboard.js",
-        config={"version": "0.1.6"},
+        config={"version": "0.3.1"},
         require_admin=False,
     )
